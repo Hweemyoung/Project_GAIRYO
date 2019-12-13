fb95055b731d163e35a9d7babb26fa24a5d6d189
hweem <hweemyoung@gmail.com>
2019-12-13 18:02:29 +0900

    CSS, JS applicable to html with multiple custom_schedules

--- js/custom_schedule.js
+++ js/custom_schedule.js
@@ -1,50 +1,4 @@
-// Screen mode
-if (window.innerWidth < 576) {
-    var screen_sm = true;
-} else {
-    var screen_sm = false;
-}
-
-if (window.innerWidth < 768) {
-    var screen_md = true;
-} else {
-    var screen_md = false;
-}
-
-// Create timeline
-const timeline_default = ['07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'];
-$(timeline_default).map(function () {
-    $('.timeline ul').append($('<li></li>').append('<span>' + this + '</span>'));
-})
-
-// Remove 2n th li object
-$('.timeline li:nth-child(2n)').css('display', 'none');
-
-// Store valid timeline texts
-var timeline_valid = [];
-var li_valid = $('.timeline ul li').filter(function () {
-    return $(this).css('display') != 'none'
-});
-li_valid.map(function () {
-    timeline_valid.push($(this).children('span').html());
-});
-
-if(screen_sm){
-    li_valid.map(function(){
-        $(this).children().html($(this).children().html().slice(0, 2));
-    })
-}
-
-// Manipulate modal
-$(document).ready(function () {
-    $('#div-columns a').click(function () {
-        $('.modal-title').html('<h1>' + $(this).children('h5').html() + '</h1>');
-        $('.modal-body').empty();
-        $(this).children('ul').clone().appendTo($('.modal-body'));
-        $('.modal').modal();
-    });
-});
-
+// RESIZE
 function resizeColumns() {
     // Distance between timestamps
     var num_horizontal_lines = timeline_valid.length;
@@ -53,22 +7,37 @@ function resizeColumns() {
 
 
     // set top of #div-columns
-    $('#div-columns').css('top', '.5rem');
+    $('.div-columns').css('top', '.5rem');
 
-    var timeline_width = $('.timeline').width();
-    var columns_left = $('#div-columns').position().left;
+    // var timeline_width = $('.card-body .row .col-md-8').width(); // Cannot use when all cards are fold.
+    var timeline_width = $('.card').width() - parseInt($('.card-body').css('padding-left')) - parseInt($('.card-body').css('padding-right'));
+    if (screen_sm) {
+        var columns_left = 20;
+    } else {
+        var columns_left = 40;
+    }
 
-    // Calc width and height of #div-columns
-    $('#div-columns').css('width', timeline_width - columns_left).css('height', $('.timeline').height());
+    // Calc width and height of .div-columns
+    // $('.div-columns').css('width', timeline_width - columns_left).css('height', $('.timeline').height());
+    // if (!screen_md){
+    //     $('.div-columns').css('width', timeline_width*2/3 - columns_left) // col-md-8
+    // } else {
+    //     $('.div-columns').css('width', timeline_width - columns_left).css('height', $('.timeline').height());
+    // }
+    if (!screen_md) {
+        $('.div-columns').css('width', timeline_width * 2 / 3 - columns_left).css('height', $('.timeline').height()); // col-md-8
+    } else {
+        $('.div-columns').css('width', timeline_width - columns_left).css('height', $('.timeline').height());
+    }
 
-    // Calc width of every column and buttons
-    var width = $('#div-columns').width() / $('.column').length;
+    // Calc width of every column and button
+    var width = $('.div-columns').width() / numColumnsPerDiv;
     $('.column').css('width', width);
     $('.column a').css('width', width);
 
     var time_head = timeline_default[0];
-    var time_head = Number(time_head.slice(0, 2)) + Number(time_head.slice(-2)) / 60
-    $('#div-columns .column a').map(function () {
+    var time_head = Number(time_head.slice(0, 2)) + Number(time_head.slice(-2)) / 60;
+    $('.div-columns .column a').map(function () {
         // Calc top
         // Get hours
         var hours_start = Number(this.getAttribute('time-start').slice(0, 2));
@@ -91,30 +60,107 @@ function resizeColumns() {
         var height = blocks * dist_horizontal;
         $(this).css('height', height);
 
-
+        // Switch screen_sm
         if (window.innerWidth < 576 && !screen_sm) {
-            li_valid.children('span').map(function (index) {
+            $('.timeline ul li').children('span').map(function () {
+                // Slice timeline texts
                 this.innerHTML = this.innerHTML.slice(0, 2);
             });
             screen_sm = true;
         } else if (window.innerWidth >= 576 && screen_sm) {
-            li_valid.children('span').map(function (index) {
-                this.innerHTML = timeline_valid[index];
+            $('.timeline ul').map(function () {
+                $(this).children('li').filter(function () {
+                    return $(this).css('display') != 'none'
+                }).map(function (index) {
+                    $(this).children('span').html(timeline_valid[index]);
+                });
+
+                // li_valid.children('span').map(function (index) {
+                //     // Timeline full texts
+                //     this.innerHTML = timeline_valid[index];
+                // });
             });
             screen_sm = false;
         }
-
-        if (window.innerWidth < 768 && !screen_md) {
-            $('.div-schedule').css('height', 500);
-            screen_md = true;
-        } else if (window.innerWidth >= 768 && screen_md) {
-            if ($('#shift-member-table').height() > 500) {
-                $('.div-schedule').css('height', $('#shift-member-table').height());
-            }
-            screen_md = false;
+    });
+    if (window.innerWidth < 768 && !screen_md) {
+        $('.div-schedule').css('height', 500);
+        screen_md = true;
+    } else if (window.innerWidth >= 768 && screen_md) {
+        if ($('.shift-member-table').height() > 500) {
+            $('.div-schedule').css('height', $('.shift-member-table').height());
         }
-    })
+        screen_md = false;
+    }
 }
 
-resizeColumns();
-$(window).resize(resizeColumns);
+// INITIALIZATION
+// Screen mode
+if (window.innerWidth < 576) {
+        var screen_sm = true;
+    } else {
+        var screen_sm = false;
+    }
+
+    if (window.innerWidth < 768) {
+        var screen_md = true;
+    } else {
+        var screen_md = false;
+    }
+
+    // Hparams
+    const numColumnsPerDiv = 3
+
+    // Create timeline
+    const timeline_default = ['07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'];
+    $(timeline_default).map(function () {
+        $('.timeline ul').append($('<li></li>').append('<span>' + this + '</span>'));
+    })
+
+    // Remove 2n th li object
+    $('.timeline li:nth-child(2n)').css('display', 'none');
+
+    // Store valid timeline texts
+    var timeline_valid = [];
+    var li_valid = $('.timeline ul li').filter(function () {
+        return $(this).css('display') != 'none'
+    });
+    li_valid.map(function () {
+        timeline_valid.push($(this).children('span').html());
+    });
+
+    // Slice timeline text when screen_sm
+    if (screen_sm) {
+        li_valid.map(function () {
+            $(this).children().html($(this).children().html().slice(0, 2));
+        })
+    }
+
+    // Manipulate modal
+    $(document).ready(function () {
+        $('.div-columns a').click(function () {
+            $('.modal-title').html('<h1>' + $(this).children('h5').html() + '</h1>');
+            $('.modal-body').empty();
+            $(this).children('ul').clone().appendTo($('.modal-body'));
+            $('.modal').modal();
+        });
+    });
+
+    // Resize
+    resizeColumns();
+
+    // EVENTS
+    $(window).resize(resizeColumns);
+    // $(document).ready(function () {
+    //     $('#accordion .card .card-header a').click(function () {
+    //         $(this).parent().siblings('.collapse').append()
+    //     });
+    // });
+
+
+    // TEMPORARY
+    // Duplicate card-body
+    $('#accordion .card .collapse:not(#day4) .card-body').map(function () {
+        $(this).replaceWith($('#day4 .card-body').clone());
+    });
+// $('#accordion .card .collapse .card-body').replaceWith($('#day4 .card-body'))
