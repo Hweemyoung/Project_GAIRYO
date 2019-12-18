d578446031baa53bd557bd020f4906fe7e346424
hweem <hweemyoung@gmail.com>
2019-12-18 22:12:28 +0900

    submit button needed

--- js/transactionform.js
+++ js/transactionform.js
@@ -12,6 +12,22 @@ function disableNoneOption(event){
     $(event.target).find('option:first-child').attr('disabled', 'disabled');
 }
 
+function checkConfirmable(){
+    var enable = 1;
+    $('select[name="id_to"]').map(function(){
+        if ($(this).attr('value') === 0 || $(this).attr('disabled') == 'disabled'){
+            enable = enable * 0;
+        } else {
+            enable = enable * 1;
+        }
+    });
+    if (enable === 0){
+        $('#btn-confirm').addClass('disabled');
+    } else {
+        $('#btn-confirm').removeClass('disabled');
+    }
+}
+
 function enableSelect(idx, $selects) {
     if(idx > $selects.length - 1){
         return
@@ -49,12 +65,51 @@ function enableSelect(idx, $selects) {
 }
 
 function addFormItem(event){
-    var newFormItem = defaultForm.clone();
-    $('form').append(newFormItem).append('<hr>');
+    numItem++;
+    var newFormItem = defaultForm.clone().attr('id', 'form-item'+String(numItem));
+    $('form').append(newFormItem);
     newFormItem.find('select').on('change', changeSelects).on('change', disableNoneOption);
+    newFormItem.find('select[name="id_to"]').on('change', checkConfirmable);
+    newFormItem.find('.btn-delete').click(deleteItem);
+    $('#btn-confirm').addClass('disabled');
+}
+
+function loadTransactions(event){
+    var modalTableBody = $('#modal-confirm .modal-dialog .modal-content .modal-body table tbody');
+    $('form .form-item').each(function(){
+        // Append tr>td*5
+        modalTableBody.append('<tr><td></td><td></td><td></td><td></td><td></td></tr>');
+        $(this).find('select').each(function(index){
+            // For each select
+            var valueSelect = this.value;
+            $(modalTableBody.find('tr:last-child td')[index]).append($(this).find('option').filter(function(){
+                return $(this).attr('value') === valueSelect
+            }).html());
+        })
+    })
+}
+
+function deleteItem(event){
+    if ($('.form-item').length > 1){
+        console.log($(event.target).closest('.form-item'));
+        $(event.target).closest('.form-item').remove();
+    }
 }
 
 const defaultForm = $('.form-item').clone();
+var numItem = 1;
 
+// Actions for selecting an option: Enable/disable selects
 $('.form-item select').on('change', changeSelects).on('change', disableNoneOption);
+// Actions for selecting an option: Enable/disable confirm button
+$('select[name="id_to"]').on('change', checkConfirmable);
+// Actions for clicking delete icon
+$('.btn-delete').click(deleteItem);
+// Actions for clicking add icon
 $('#btn-add-item').click(addFormItem);
+// Actions for clicking confirm icon: Load details to modal body
+$('#btn-confirm').click(loadTransactions);
+// Actions for closing modal: Empty table body
+$('#modal-confirm').on('hidden.bs.modal', function(event){
+    $(event.target).find('.modal-body table tbody').empty();
+});
